21fe755ba62af8c0d363db52a5b81adbc05732fe
minjee412 <minjee.412@kakao.com>
2021-11-18 03:22:12 +0900

    (추가) 탭네비게이션 & 영화페이지

--- pages/navigation/TabNavigator.tsx
+++ pages/navigation/TabNavigator.tsx
@@ -2,47 +2,56 @@ import * as React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MainListNavigation from './MainListNavigation'
 import MyPageNavigation from './MypageNavigation'
-import Ionicons from "react-native-vector-icons/Ionicons"
-import { Image } from "react-native"
-import MovieNavigation from './MovieNavigation'
+// import { Image } from "react-native"
+import Movies from '../screens/Movie/MoviePage'
+import { useColorScheme } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
 export default function TabNavigator () {
     const Tab = createBottomTabNavigator()
+    const isDark = useColorScheme() === 'dark';
+    
 
     return (
-        <Tab.Navigator initialRouteName="MainList" screenOptions={{tabBarActiveTintColor:'#518099'}} >
+        <Tab.Navigator initialRouteName="MainList" screenOptions={{
+                tabBarActiveTintColor: isDark ? "#ffbe0b" : '#518099',
+                tabBarInactiveTintColor: isDark ? "gray" : "gray" ,
+                tabBarLabelPosition:'below-icon', 
+                tabBarStyle:{ backgroundColor: isDark ? "black" : "white"},
+                tabBarLabelStyle:{
+                    fontSize:12,
+                    fontWeight:'500',
+                    marginTop:-5
+                }
+        }} >
+
             <Tab.Screen name="MainList" 
                 component={MainListNavigation}
-                options={{headerShown:false, tabBarIcon:({size})=> {
+                options={{headerShown:false, tabBarIcon:({focused, color, size})=> {
                     return (
-                        <Image
-                            style={{width:size, height:size }}
-                            source={require('../../public/images/List/checkList.png')}
-                        />
+                        <MaterialCommunityIcons name={focused ? 'clipboard-list' :'clipboard-list-outline'} color={color} size={size} />
                     )
                 }}}
             />
+
+            <Tab.Screen name="Movie"
+                component={Movies} 
+                options={{headerShown:false,tabBarIcon:({focused, color, size})=> {
+                    return (
+                        <Ionicons name={focused ?'film' : 'film-outline'} color={color} size={size} />
+                    )
+                }}}
+            />
+
             <Tab.Screen name="MyPage"
                 component={MyPageNavigation} 
-                options={{tabBarIcon:({size})=> {
+                options={{tabBarIcon:({focused, color, size})=> {
                     return (
-                        <Image
-                            style={{width:size, height:size }}
-                            source={require('../../public/images/List/user.png')}
-                        />
-                    )
-                }}}
-            />
-            <Tab.Screen name="Movies"
-                component={MovieNavigation} 
-                options={{headerShown:false,tabBarIcon:({size})=> {
-                    return (
-                        <Image
-                            style={{width:size, height:size }}
-                            source={require('../../public/images/MovieIcon/movieImg.png')}
-                        />
+                        <FontAwesome name={focused ?'user' : 'user-o'} color={color} size={size} />
                     )
                 }}}
             />
