be387b86f50a45c5147a8e37d282e07d8b73691e
lavercourage <smqkemfht@gmail.com>
2021-11-18 12:44:31 +0900

    (수정) 날짜 불러오기
    feature-issue-#52

--- pages/screens/Calendar/index.tsx
+++ pages/screens/Calendar/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import CalendarView from "../../../src/component/Calendar/CalendarView"
+import LogContext from "../../../contexts/LogContext"
+import { format } from 'date-fns';
 
-export default function CalendarScreen({navigation}){
+function CalendarScreen({navigation}){
+    const {logs}:any = useContext(LogContext);
+    const [selecteDate, setSelectedDate] = useState(
+        format(new Date(), "yyyy-MM-dd"),
+    )
 
-    return <CalendarView />
+    const markedDates = logs.reduce((acc, current) => {
+        const formattedDate = format(new Date(current.date), "yyyy-MM-dd");
+        acc[formattedDate] = {marked: true};
+        return acc;
+    }, {});
+
+    return (
+        <CalendarView
+            markedDates={markedDates}
+            selecteDate={selecteDate}
+            onSelectDate={setSelectedDate}
+        />
+    );
 };
 
+export default CalendarScreen;
