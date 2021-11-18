0be90afac215fe4625ff61a9ad37e18d5015468c
중현 <jhlcoder@gmail.com>
2021-11-18 19:21:42 +0900

    (추가):마이페이지 등록

--- pages/screens/Detail/index.tsx
+++ pages/screens/Detail/index.tsx
@@ -15,30 +15,33 @@ import {
     BodyContentEmpty,
     BodyText,
     NullWrapper,
-    Footer,
-    InnerFooter,
-    InputBar,
-    SendIcon,
 } from "./Detail.styles"
 import { TouchableOpacity } from "react-native"
 import MemoWrite from "../../../src/component/memo/memoWrite"
 import auth from '@react-native-firebase/auth'
 import firestore from '@react-native-firebase/firestore'
-import {v4} from 'uuid'
 
 
-
-function Detail({navigation, route, props}, ){
-    console.log("aaa", route)
-    const [content, setContent] = useState("")
-    const ID = v4();
-
-    console.log("디테일페이지 프롭스: ",route)
+function Detail({navigation, route}, ){
 
     const user = auth().currentUser;
     const aaa = [];
     const [ detailV, setDetailV ] = React.useState([])
 
+    const Submit = async () => {
+        try {
+            const result = await firestore()
+                .collection("Users")
+                .doc(user.uid)
+                .collection("Todo")
+                .doc(ID )  // <=========== 해당 괄호 안에 들어갈 값.. 구할 수가 없어요..!
+                .delete()
+                alert("삭제완료")                
+        } catch (error) {
+            console.log(error)
+            alert("삭제실패")
+        }
+    }
     // React.useEffect(() => {
     //     const doc = firestore()
     //         .collection("Users")
@@ -58,57 +61,6 @@ function Detail({navigation, route, props}, ){
     // console.log("가나다", firestore().collection("Users").doc("").collection("Todo").get())
     // console.log("props: ", props.item.title)
     // console.log("aaa", route.params)
-
-
-
-        // 글 삭제
-    const Submit = async () => {
-        try {
-            const result = await firestore()
-                .collection("Users")
-                .doc(user.email)
-                .collection("Todo")
-                .doc("74XKZB9reJJePNpNjbYL")
-                .delete()
-                alert("삭제완료")                
-        } catch (error) {
-            console.log(error)
-            alert("삭제실패")
-        }
-    }
-
-
-    //     // 글 삭제
-    // const Submit = async () => {
-    //     try {
-    //         const result = await firestore()
-    //             .collection("Users")
-    //             .doc(user.email)
-    //             .collection("Todo")
-    //             .doc(id)
-    //             .delete()
-    //             alert("삭제완료")                
-    //     } catch (error) {
-    //         console.log(error)
-    //         alert("삭제실패")
-    //     }
-    // }
-
-    // // 메모 등록
-    // const Submit = () => {            
-    //     firestore()
-    //         .collection('Users')
-    //         .doc(user.email)
-    //         .collection("Todo")
-    //         .doc(route.params.id)
-    //         .collection("4")
-    //         .add({
-    //             content,
-    //             ID,
-    //             createdAt: new Date
-    //         })
-    // }
-
 
     return(
         <>
@@ -133,25 +85,9 @@ function Detail({navigation, route, props}, ){
                                     {route.params.place ? <BodyText>{route.params.place}</BodyText> : <NullWrapper/> }
                                     {route.params.important ? <BodyText>{route.params.important}</BodyText> : <NullWrapper/> } 
                                 </BodyMiddel>
-                                <BodyBottom>
-                                    {/* <Memo>{route.params.id}</Memo> */}
-                                </BodyBottom>
+                                <BodyBottom></BodyBottom>
                 </SafeAreaTop>
-                {/* <MemoWrite /> */}
-                <Footer>
-                    <InnerFooter> 
-                        <InputBar 
-                            placeholder="댓글을 입력하세요"
-                            placeholderTextColor={"#888888"}
-                            maxLength={100}
-                            value={content}
-                            onChangeText={setContent}
-                        />
-                        {/* <TouchableOpacity onPressOut={MemoSubmit}>
-                            <SendIcon source={require("../../../public/images/List/send.png")}/>
-                        </TouchableOpacity> */}
-                    </InnerFooter>
-                </Footer>
+                <MemoWrite/>
             </SafeArea>
         </>
     )
---
+++ pages/screens/UserInformation/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import UserInformationPage from '../../../src/component/UserInformation/userInformaiton.container'
+
+export default function UserInformation () {
+    return <UserInformationPage />
+}
